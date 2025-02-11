6c6295348b7fa54eaf03370ded765e4eea1a4f65
Prayas Gupta <shuklaaayush805@gmail.com>
2025-02-12 00:45:04 +0530

    Create precision.c

---
+++ precision.c
@@ -0,0 +1,31 @@
+
+#include <stdio.h>
+ 
+ 
+int main(void) {
+ 
+ 
+	//double has more precision than float ,float has precision upto 6 decimal number while double has precision upto 14/15 decimal numbers
+ 
+	//long double has more precision than double
+ 
+//after 6 digit float gives us garbage value
+ 
+ 
+float f1=9.122334;
+ 
+printf("%f",f1);
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+}
+ 
