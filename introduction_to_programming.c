56d9a7ae585fb9cd496d420cdc8bb97d0da25a73
Prayas Gupta <shuklaaayush805@gmail.com>
2025-02-12 00:42:39 +0530

    Create introduction_to_programming.c

---
+++ introduction_to_programming.c
@@ -0,0 +1,46 @@
+public class InttroductionToProgramming {
+    A computer ->its a device which xconsisit of multiple devices sepcially storage devuices like RAM,ROM
+    Process devices->its used for doing processing of the data like CPU which is used to add substract things written in a file.
+    Ouput devices -> which is used to display the output on the screen eg like LED.
+
+        why computer pgrm exists
+            because if no computer pgrm exist then who will tell compuyter how to execute the task.
+
+    The code or the program that you write is a software.
+
+        //we can short an array of smaller size manually
+    //but it will take a lot opf time for solving for millions of size
+    //thats why we need computer pgrm as they are faster
+    //computer pgrm are structured and have a termination condition
+    //while solving problm manually doesnt have structured and termination condition.
+    //as computer can understand only 0's and 1's we have programming language like C++,java for communicating with computer.
+    //Compiler is used to C++,java into low level binary code.
+    //why our local language isnt applicable because its ambigous in nature.
+//for computer pgrm to read an input generally its done through stdin .and give outpout through stdout
+    //there ares some other ways as well
+    //file ,network through which it takes input
+    //and write output into another file and network.
+
+//
+//    firstly we compile our code which then produces executable format and then we run it.
+//    IDE providez a platoform where we can write our java /C++ code and can run and compile our code througb compile and run button.
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
