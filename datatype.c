ca0b8331aa425bef37359a5ea8025eafea0f1e9d
Prayas Gupta <shuklaaayush805@gmail.com>
2025-02-12 00:41:51 +0530

    Create datatype.c

---
+++ datatype.c
@@ -0,0 +1,66 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+//naming convention while decalring data type name
+	//shouuldnt be keyword
+	//shouldnt have space
+	//shouldnt be arithematic operator lie, that
+	//shouldnt have multiple variable with same name
+
+
+
+	int a;
+	int A;
+
+//	printf("%d\n%d",a,A);
+
+
+	//cant have like this int +a;
+//	or like this
+
+//	int float a=12;XXXXX not allowded
+
+
+
+//	cant have int a b=213;
+
+	 int b1=23;//can have
+
+//also cant have naming starting with number
+
+
+
+
+//we can have underscore first
+
+
+	 int _a=2334;
+
+
+	 printf("%d",_a);
+
+printf("\n");
+
+	 int $a=2334;
+
+
+	 	 printf("%d",$a);
+
+
+
+	 	 int #a=2334;
+
+
+	 		 	 printf("%d",#a);//will give error
+
+
+	//will give randowm garbage value in c while in java would haev given compilation error
+
+
+
+	return 0;//
+}
