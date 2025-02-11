22cbca24d8c5652ecb6ee600dcc25ed352210cdd
Prayas Gupta <shuklaaayush805@gmail.com>
2025-02-12 00:46:28 +0530

    Create formatspecifier.c

---
+++ formatspecifier.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+ 
+int main(void) {
+ 
+ 
+//format specifier->use right format specifier for right variable.
+//there are two types of printf();
+ 
+ 
+//one that takes format specifier
+//other that takes String value as input
+ 
+int a=23;
+char c='0';
+double d=23.343423;
+printf("%d%c%lf",a,c,d);
+ 
+ 
+ 
+ 
+ 
+	return 0;
+}
+ 
