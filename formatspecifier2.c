77e57c57c8442e517bbf1df35636015b58c09caa
Prayas Gupta <shuklaaayush805@gmail.com>
2025-02-12 00:49:41 +0530

    Create formatspecifier2.c

---
+++ formatspecifier2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+ 
+int main(void) {
+	// arithematic operator
+ 
+	int a;
+	float b;
+ 
+	scanf("%d%f",&a,&b);
+ 
+ 
+ 
+//	printf("%d",(a/b));//here will give abrupt behaviour
+ 
+printf("%f",(a/b));	
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+	return 0;
+}
+ 
