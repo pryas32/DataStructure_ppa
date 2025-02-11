bda41c8bba261ecd0b9cb603cfcddd8aedd58271
Prayas Gupta <shuklaaayush805@gmail.com>
2025-02-12 01:06:51 +0530

    Create typecasting.c

---
+++ typecasting.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+ 
+int main(void) {
+ 
+int a=12;
+float b=12.5;
+ 
+ 
+
+ 
+//printf("%f",a-b);
+ 
+printf("%d",a*b);//float *int =float ;
+float/int =float as float covers larger space value in comparision to integer so 
+here will give error 
+
+//while in case of float ie %f will not have given error . ie  
+//printf("%f",a-b);
+ 
+ 
+ 
+ 
+	return 0;
+}
+ 
