468a888eccf24d8ccba2220abf2f2418a67090de
Prayas Gupta <shuklaaayush805@gmail.com>
2025-02-12 00:44:05 +0530

    Create C.c

---
+++ C.c
@@ -0,0 +1,8 @@
+
+#include <stdio.h>//.h indicates its a header file it contains all the functions.functions related to taking an input through scanf() and printing output on console through printf();
+
+int main() {// this is where from the execuition of the pgrm begins main is a function which doesnt takes any input and return output
+//math.h-> which is used to contain library functions realted to math.
+	printf("hello world");
+	return 0;///
+}
