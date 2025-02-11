05ebc691b94c9fcf06d9ea9fc46f2dd473e7f661
Prayas Gupta <shuklaaayush805@gmail.com>
2025-02-12 00:59:16 +0530

    Create assignmentoperator.c

---
+++ assignmentoperator.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int main(void) {
+//assigment operator-> should be in conjuction of each other
+//+=,-=,%=,*=
+int a=3;
+a+=1;
+a*=1;
+a%=3;
+ 
+ 
+ 
+printf("%d",a);
+ 
+	return 0;
+}
+ 
