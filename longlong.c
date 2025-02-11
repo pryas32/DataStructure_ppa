692d4945c8b6cc9b40c8f550682a2ed6452ff1d9
Prayas Gupta <shuklaaayush805@gmail.com>
2025-02-12 00:47:18 +0530

    Create longlong.c

---
+++ longlong.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ 
+int main(void) {
+ 
+ 
+long long fl=1000000000000;
+ 
+ 
+printf("%d",fl);//it end up giving abrupt answer
+ 
+ 
+ 
+ 
+	return 0;
+}
+ 
+Success #stdin #stdout 0s 5276KB
+-727379968
