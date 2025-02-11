a0108d7c67a8bc6117e9552401a0a95b71f6a90c
Prayas Gupta <shuklaaayush805@gmail.com>
2025-02-12 00:47:47 +0530

    Create longlong2.c

---
+++ longlong2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ 
+int main(void) {
+ 
+ 
+long long fl=1000000000000;
+ 
+ 
+printf("%d",fl);//it end up giving abrupt answer
+ 
+printf("abc");//it takes it as a String
+ 
+int x;
+printf(x);//gives error
+ 
+ 
+	return 0;
+}
