6ee8e562ff3f56f7e3c3a6ff06f4c00c2302aa5c
Prayas Gupta <shuklaaayush805@gmail.com>
2025-02-12 00:56:55 +0530

    Create &&operator.c

---
+++ &&operator.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+ 
+int main(void) {
+//apart from 0 elsething is true
+ 
+ 
+//logical operator ->&&,||,!;
+ 
+ 
+int a=12;
+int b=12;
+int c=13;
+ 
+ 
+int d=a>=b && b>=c;
+ 
+printf("%d",d);
+ 
+ 
+int d1=-5&&5;
+ 
+ 
+printf("\n");
+ 
+printf("%d",d1);
+ 
+ 
+printf("\n");
+ 
+int e=-5+5&& 5;
+ 
+ 
+ 
+ 
+printf("%d",e);
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+	return 0;
+}
+ 
