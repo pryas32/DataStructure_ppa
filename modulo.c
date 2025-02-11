7c1a67fd8f1db4df29e69070e656448fde669df7
Prayas Gupta <shuklaaayush805@gmail.com>
2025-02-12 01:07:26 +0530

    Create modulo.c

---
+++ modulo.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+ 
+int main(void) {
+//modulo operator
+ 
+ 
+//devidend =devisor*quotient+remainder;
+//like 14=4*2+2;
+//points to be kept in mind
+ 
+ 
+//remainder tells the distance from the nearest multiple like eg 13%4=1 means that its distance from nearest multiple ie 12 ie 1.
+ 
+ 
+//if we devide 14%0 then it will give error 
+// modulo of negative number is little bit shaky we can come up with remainder by removing the -ve sign.
+ 
+//modulo of decimal or floating point is not possible as quotient sometimes become infinity repeating
+ 
+ 
+ 
+ 
+int a=-14%3;
+printf("%d",a);
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+	return 0;
+}
+ 
