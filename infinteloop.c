733c1003d41a61c9c0e56d462eb54824fc07d094
Prayas Gupta <shuklaaayush805@gmail.com>
2025-02-12 23:18:45 +0530

    Create infinteloop.c

---
+++ infinteloop.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) {
+
+
+
+//we can make loop run infinitely either we forgot about incresment variable .
+//or pass condition which never makes loop condition true .
+
+int i=0;
+while(1){//pass 0 or 1 =>1 means true 0 means false everything apart from 0 is true.
+	printf("hello world");
+	i++;
+}
+
+
+
+
+
+
+
+	return 0;
+}
