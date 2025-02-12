d290532c38671f568df7cedd3a2064ae24cff5c7
Prayas Gupta <shuklaaayush805@gmail.com>
2025-02-12 23:30:25 +0530

    Create multiplication.c

---
+++ multiplication.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void) {
+
+//printing odd and even;
+
+
+int n;
+scanf("%d",&n);
+
+int i=1;
+int a=2;
+while(i<=n)
+{
+printf("2X%d=%d",i,a*i);
+printf("\n");
+i++;
+}
+
+
+
+
+
+
+
+
+	return 0;
+}
