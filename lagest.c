20b346f21f4a03b06949007ef5a8d2b881c5eafd
Prayas Gupta <shuklaaayush805@gmail.com>
2025-02-12 11:25:45 +0530

    Create lagest.c

---
+++ lagest.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+ 
+int main(void) {
+ 
+int x=12,y=13,z=14;
+ 
+ 
+ 
+if(x>=y && x>=z)
+{
+	printf("%d",x);
+}else if(y>=x && y>=z)
+{
+	printf("%d",y);
+}
+else{
+	printf("%d",z);
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+	return 0;
+}
+ 
