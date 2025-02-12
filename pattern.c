33c00bb7c0048ec27ab87a3633a9c180b07b4546
Prayas Gupta <shuklaaayush805@gmail.com>
2025-02-13 00:02:06 +0530

    Create pattern.c

---
+++ pattern.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) {
+
+//printing odd and even;
+
+
+int n;
+scanf("%d",&n);
+
+
+while(n!=0)
+{
+	printf("%d",n);
+	n=n/10;
+	printf("\n");
+}
+
+
+
+
+	return 0;
+}
