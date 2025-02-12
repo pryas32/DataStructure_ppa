8a5da4ff8761e1439b4091d2799d1ef0d6d3fdab
Prayas Gupta <shuklaaayush805@gmail.com>
2025-02-12 23:21:11 +0530

    Create priint1ton.c

---
+++ priint1ton.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(void) {
+
+//printing natural number from 1 to n;
+
+
+int n;
+scanf("%d",&n);
+
+
+int i=1;
+while(i<=n)
+{
+	printf("%d",i);
+	i++;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
