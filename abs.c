25bf22e2c56abf4cd8b58947369fab0e7544e402
Prayas Gupta <shuklaaayush805@gmail.com>
2025-02-14 01:30:10 +0530

    Create abs.c

---
+++ abs.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include<stdlib.h>//used for distance of integer from 0//always gives positive distance
+
+int main(void) {
+int a=abs(2-2);
+int y=abs(7-10);
+printf("%d",a);
+printf("%d",y);
+
+
+}
