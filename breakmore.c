b1e4fb707d998877934b2b3ec137f1ee7d308061
Prayas Gupta <shuklaaayush805@gmail.com>
2025-02-14 00:16:11 +0530

    Create breakmore.c

---
+++ breakmore.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+//more about break keyword
+
+
+
+int i=0;
+
+
+while(i<=4)
+{
+    if(i==3)
+    {
+        break;
+    }
+    
+    printf("happy world");
+    printf("\n");
+    i++;
+    
+}
+
+
+
+}
+
+
+
