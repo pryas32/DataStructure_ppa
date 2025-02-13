0aa0c293a4e128da82d7b34888035302af96ffe8
Prayas Gupta <shuklaaayush805@gmail.com>
2025-02-14 00:23:12 +0530

    Create forloop.c

---
+++ forloop.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void) {
+
+//for loop -> it has three conditions
+
+//initialization ,comparing ,increwment all three fields are optional
+
+for(;1;)
+{
+	printf("Prayas");//it will run infinitely.
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
