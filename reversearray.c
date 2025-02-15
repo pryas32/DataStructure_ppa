aff37d44e900c4be68d3d136b4d13d453e9c5cca
Prayas Gupta <shuklaaayush805@gmail.com>
2025-02-15 17:43:55 +0530

    Create reversearray.c

---
+++ reversearray.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void) {
+
+
+
+
+
+int arr[]={1,2,3,4,5,6};
+
+//printing in reverse order
+
+for(int i=5;i>=0;i--)
+{
+	printf("%d\n",arr[i]);
+}
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
