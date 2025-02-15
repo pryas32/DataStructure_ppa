10da8a424076b612a3b5f42dabcfc95ed212a44a
Prayas Gupta <shuklaaayush805@gmail.com>
2025-02-15 16:57:58 +0530

    Create array.c

---
+++ array.c
@@ -0,0 +1,17 @@
+//array -> its a collection of elements of same data type.
+
+// we cant decalre array of negative size // we cant access array[-1];
+// we cant have array of arrayindexoutofbound.
+
+// we can store upto 10^6 elemtn in int size array
+// for storing value greater than int we have long long for that 
+//long long array[10^23];
+//for storing INT_MIN ,LLONG_MIN,Long_MAX,we have #include<limits.h>
+
+
+
+
+
+
+
+
