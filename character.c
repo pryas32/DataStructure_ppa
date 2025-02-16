3df0307dda2a32acec1dfd6244a1eddc3abfe877
Prayas Gupta <shuklaaayush805@gmail.com>
2025-02-16 22:21:23 +0530

    Update character.c

--- character.c
+++ character.c
@@ -76,7 +76,31 @@ a b c   d e f   h  //as it count spaces also as character
     return 0;
 }
 
+/******************************************************************************
 
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    //char ch[100];
+    //whenver taking any character array as input 
+    //declare of one more than size as to keep last character as '\0'
+    
+    
+    char ch[10]={'a','b','c','d','e','f','g','h','i','j'};
+    
+    for(int i=0;ch[i]!='\0';i++)
+    printf("%c",ch[i]);
+    
+
+    return 0;
+}
 
 
 
