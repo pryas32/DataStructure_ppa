6811671ae553440d969fa93ae3577d4a2feb2a45
Prayas Gupta <shuklaaayush805@gmail.com>
2025-02-18 14:51:22 +0530

    Create operationsonpointers.c

---
+++ operationsonpointers.c
@@ -0,0 +1,48 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    //pointer arithemetic and substraction are possible but pointer division and multiplication not possible as they dont have any significance on number line
+    
+    
+    
+    //operations that we can perform on pointers itself
+    
+    
+    //for two pointers we can compare them , we can substract them , we cant add them or * or / them .
+    
+    
+    int arr[]={1,2,3,4};
+    
+    
+    
+    
+    
+    int *p=arr; 
+    
+    
+    int *p1=&arr[3];
+    
+    
+    
+    int d1=(p1-p);
+    
+    
+    
+    
+    printf("%d",d1);
+    
+    
+    
+    
+    
+    return 0;
+}
