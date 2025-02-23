27089022f0114d96c1178a20a9b7da753d61ca2c
Prayas Gupta <shuklaaayush805@gmail.com>
2025-02-23 22:19:26 +0530

    Create divisors.java

---
+++ Data_Structure/Arrays&DynamicArrays/divisors.java
@@ -0,0 +1,47 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		//finding divisors of n 
+		//using optimized approach      //O(N) Time Complexity
+		int n;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		//simple approach
+		
+		
+		for(int i=1;i<=n;i++)
+		{
+			if(n%i==0)
+			{
+				System.out.println(i);
+			}
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
