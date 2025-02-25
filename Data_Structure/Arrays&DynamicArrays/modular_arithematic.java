7ae10eb197d06547af7ebae3de9005ec10aefc78
Prayas Gupta <shuklaaayush805@gmail.com>
2025-02-25 21:57:44 +0530

    Create modular_arithematic.java

---
+++ Data_Structure/Arrays&DynamicArrays/modular_arithematic.java
@@ -0,0 +1,34 @@
+ /* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		//modular arithemetic
+		
+		
+		
+		(a+b)%m=>(a%m+b%m)%m;  why because a%m=>{0 to m-1} and b%m=>{0 to m-1} so min and maxi value 0 to 2m-1 which is wrong
+		//so we have to tak modulo of that 
+		
+		
+		//similarly for (a-b)%m=>(a%m-b%m)%m;-> for a%m o to m-1 and b%m=>u o to m-1 min value -m+1 and maximu value m-1
+		
+		-m+1 to m-1 
+		 we will neglect -m+1 value so range will be from 0 to m-1
+		 
+		 if negative remainder then +m kardo
+		 
+		 (a*b)%m=>((a*m*b*m))*m;
+		 
+		 
+		 
+		
+		
+	}
+}
