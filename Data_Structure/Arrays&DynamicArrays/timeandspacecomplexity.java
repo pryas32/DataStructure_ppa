df83ba790317d3399566dfca37fa8b535793919d
Prayas Gupta <shuklaaayush805@gmail.com>
2025-02-28 00:01:14 +0530

    Create timeandspacecomplexity.java

---
+++ Data_Structure/Arrays&DynamicArrays/timeandspacecomplexity.java
@@ -0,0 +1,79 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	space complexity-> it is the space taken by extra variable which are decalred .in this the input space is not considered
+	
+	
+	eg for(int i=0;i<n;i++)
+	{
+		int sum=0;O(1) extra space taken
+		
+		int arr[n];extra space taken O(n);
+		
+		
+		
+		space complexity -> for iterative code we need to see for extra variabke declared 
+		
+		
+		while in case of recrusive we need to see for stack size as well as extra variable decalred.
+		
+		in case of time complexity dirtectly proportional to no of operations directly proportional to input size.
+		
+		
+		inner loop if dependent on outer loop should be able to find the pattern how inner loop is changing comparison to outer loop.
+
+there are two ways to find time complexity ->one throgh loop 
+2. through recurrence equation.
+		
+		
+		
+		
+		//some special series
+		
+		N
+		n/2;
+		n/2^2
+		n/2^3
+		
+		
+		
+		
+		so N/2^x=1
+		
+		N=log2N;
+		
+		
+		
+		sum from [1/2+1/3+1/5+1/7.....]   O(loglog(n)//reciprocal of primes
+		
+		
+		sum from [1/1+1/2+1/3+1/4.......]  log(n)//reciprocal of natural numbers
+		
+		1+1/2+1/4+1/8+1/16+..... a/1-r;
+		
+		
+		a=refers to first term
+		r=refers to common ratio.
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	}
+}
