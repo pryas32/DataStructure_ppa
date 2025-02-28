777c1593696a8cbbf23bf8548752fdc991cf0c60
Prayas Gupta <shuklaaayush805@gmail.com>
2025-02-28 23:42:33 +0530

    Create recursionrules.java

---
+++ Data_Structure/Arrays&DynamicArrays/recursionrules.java
@@ -0,0 +1,39 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+	//while writing recursion program we need to keep something in mind
+	
+	
+	//class name
+	//argument type if value is not changing that argument can be kept as global
+	//return type can be void  ->its in regard with return type of function being called.
+	
+	
+	//talking about calling function
+	//recursive equation
+	//termination equation->if not kept termination condition then will leads to stackoverflow condition.
+	//function which is calling
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+	
+	
+	
+	
+}
