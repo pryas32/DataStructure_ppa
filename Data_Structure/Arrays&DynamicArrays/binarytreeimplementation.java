18a157297f29fd1fea8985592982b3e11fa51a0a
Prayas Gupta <shuklaaayush805@gmail.com>
2025-03-23 17:00:23 +0530

    Create binarytreeimplementation.java

---
+++ Data_Structure/Arrays&DynamicArrays/binarytreeimplementation.java
@@ -0,0 +1,21 @@
+//binary tree implementation
+
+
+//binary tree implementation
+
+
+class Node{
+	int data;
+	Node left;
+	Node right;
+	
+	
+	Node(int data )
+	{
+		this.data=data;
+		left=null;
+		right=null;
+	}
+	
+	
+}
