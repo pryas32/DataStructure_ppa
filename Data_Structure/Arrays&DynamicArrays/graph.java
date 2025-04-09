54b9c3bb12f9c4cb8df5729697012891d95f921d
Prayas Gupta <shuklaaayush805@gmail.com>
2025-04-10 00:30:49 +0530

    Create graph.java

---
+++ Data_Structure/Arrays&DynamicArrays/graph.java
@@ -0,0 +1,53 @@
+//graph introduction
+
+
+graph->
+
+linear data structure->arrays ,vector
+linear data structure with variable size->linedlist ,doublylinkedlist ,circularlinkedlist
+linear data strucrture with special funxtions->stack and queues;
+pair data sttructures->consisting of keys and values
+hashmap,treemap linkedhashmap;
+herarchical ds
+trees 
+bst
+heaps
+queues
+A tree having cycle we can move from one node to another one
+every tree is a grapgh which doesnt have a cycle
+edge->it is a path between the nodes of a graph
+directed graph->which has a direction liuke A follows b A->b
+undirected graph-> which oesnt have any directon
+here a follows b and b dolows a .an undirected graph can be represented as directed graph
+A->B
+and B->Aunweighted graph-> graph which doesnt have any weight is known as unweighted graph.
+weighted graph->graph which are having weighted associated with the edges.
+complete graph-> graph having egge between every nodes.
+if n is no of nodes then no of edges->nC2 
+for a bidirectional graph we have n(n-1) edges.
+reachable nodes-> if there is a path reaching from node a to node b
+known reachable nodes
+unreachable nodes-> when there is no path moving from one edge to another one
+1 connected component->if we can move from one node to another node and no other node from outside would be able to reach that is reagrded as one connected component.
+self loop-> if there is a node which connects itself to itself.
+
+parallel edges-> if there are more than one edge between two nodes they are known as parallel edges.
+cycle in graph-> if there is one path which is non repeating one from where we can reach to the same node that path is reagrded as cyclic path
+
+self loop has a cyclic path.
+Bipartite graph-> if a graph can be colored using two color such that no two adjacent nodes has the same color is regarded as bipartite graph
+even node are not bipartite one
+degree of a node->refers to no of adjacent edges connected to that nodes.
+self loop add two degree to a node
+while an edges contribuite +1 to each node
+hence degree=2* no of edges.
+in case of connected dirtetional graph we have no of edges=n(n-1)
+
+in case of directed graph-> we have indegree and outdegree
+no of nodes coming into the nodes->known as indegree->n
+no of nodes going out of nodes-> known as outdegree->n
+
+
+in case of complete graph degree of a node is->no of nodes-1;
+
+
